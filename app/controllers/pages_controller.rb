ed3840ef62917b226308852f7342d148c179581d
rokkit <fss92@pochta.ru>
2012-10-10 22:29:47 +0400

    Add tweet button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,9 @@ class PagesController < ApplicationController
     answers = Array.new(3) {|i| params["quest_#{i}"]}
     answer = Answer.new  q1: params['quest_1'], q2: params['quest_2'], q3: params['quest_3'], q4: params['quest_4']
     if answer.save
-      render text: "ok"
+      respond_to do |format|
+         format.js {}
+      end
     else
       render text: "not"
     end
