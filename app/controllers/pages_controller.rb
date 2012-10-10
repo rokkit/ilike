325ccb87176bfab0adfd66cd623963e2f46ccb13
rokkit <fss92@pochta.ru>
2012-10-10 20:13:41 +0400

    Add create ctrl

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,9 @@ class PagesController < ApplicationController
   def index
   end
 
-  def controller
+  def create
     render :nothing => true
+    answers = Array.new(3) {|i| params["quest_#{i}"]}
+
   end
 end
