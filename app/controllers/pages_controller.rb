07b8f52f1600aa32ac4ef066258c8451aa9d4406
rokkit <fss92@pochta.ru>
2012-10-10 21:09:22 +0400

    adding anwwers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,16 @@ class PagesController < ApplicationController
   end
 
   def create
-    render :nothing => true
     answers = Array.new(3) {|i| params["quest_#{i}"]}
+    answer = Answer.new  q1: params['quest_1'], q2: params['quest_2'], q3: params['quest_3'], q4: params['quest_4']
+    if answer.save
+      render text: "ok"
+    else
+      render text: "not"
+    end
+  end
 
+  def list
+    @answers = Answer.all
   end
 end
